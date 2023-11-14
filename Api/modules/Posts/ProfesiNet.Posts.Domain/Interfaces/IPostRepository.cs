77b10c0b5cffac141aee3ee0fea326fcb279f38a
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2023-11-14 22:06:16 +0100

    Added Generic repository

---
+++ Api/modules/Posts/ProfesiNet.Posts.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using ProfesiNet.Posts.Domain.Entities;
+
+namespace ProfesiNet.Posts.Domain.Interfaces;
+
+public interface IPostRepository
+{
+    Task<Post?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<IQueryable<Post>> GetAllAsync(CancellationToken ct, Expression<Func<Post, object>>? include = null);
+    Task<Guid> AddAsync(Post entity, CancellationToken ct);
+    Task UpdateAsync(Post entity, CancellationToken ct);
+    Task DeleteAsync(Post entity,  CancellationToken ct);
+    Task<Post?> GetRecordByFilterAsync(Expression<Func<Post, bool>> filter, CancellationToken ct);
+    Task<IEnumerable<Post?>> GetAllForConditionAsync(Expression<Func<Post, bool>> filter, CancellationToken ct);
+    Task<bool> AnyAsync(Expression<Func<Post, bool>> predicate, CancellationToken ct);
+}
---
+++ Api/modules/Posts/ProfesiNet.Posts.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using ProfesiNet.Posts.Infrastructure.Persistence;
+
+namespace ProfesiNet.Posts.Infrastructure.Repositories;
+
+public class GenericRepository<TEntity, TKey>  where TEntity : class
+{
+    private readonly ProfesiNetPostDbContext _dbContext;
+    private readonly DbSet<TEntity> _entities;
+
+    public GenericRepository(ProfesiNetPostDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _entities = _dbContext.Set<TEntity>();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(TKey id, CancellationToken ct) => await _dbContext.Set<TEntity>().FindAsync(new object?[] { id, ct }, cancellationToken: ct);
+
+    public async Task<IQueryable<TEntity>> GetAllAsync(CancellationToken ct, Expression<Func<TEntity, object>>? include = null)
+    {
+        IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+
+        if (include != null)
+        {
+           query = query.Include(include);
+        }
+
+        var result = await query.ToListAsync(ct);
+        return result.AsQueryable();
+
+    }
+    public async Task<Guid> AddAsync(TEntity entity, CancellationToken ct)
+    {
+        await _dbContext.Set<TEntity>().AddAsync(entity, ct);
+        await _dbContext.SaveChangesAsync(ct);
+        var property = _dbContext.Entry(entity).Property("Id");
+        return (Guid)(property.CurrentValue ?? throw new InvalidOperationException());
+    }
+
+    public async Task UpdateAsync(TEntity entity, CancellationToken ct)
+    {
+        _dbContext.Set<TEntity>().Update(entity);
+        await _dbContext.SaveChangesAsync(ct);
+    }
+
+    public async Task DeleteAsync(TEntity entity,  CancellationToken ct)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync(ct);
+    }
+    public async Task<TEntity?> GetRecordByFilterAsync(Expression<Func<TEntity, bool>> filter, CancellationToken ct)
+    {
+        return await _entities.Where(filter).FirstOrDefaultAsync(ct);
+    }
+    public async Task<IEnumerable<TEntity?>> GetAllForConditionAsync(Expression<Func<TEntity, bool>> filter, CancellationToken ct)
+    {
+        return await _entities.Where(filter).ToListAsync(ct);
+    }
+    public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct)
+    {
+        return await _dbContext.Set<TEntity>().AnyAsync(predicate, ct);
+    }
+}
