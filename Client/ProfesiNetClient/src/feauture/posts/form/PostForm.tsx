912a00b0645275a33e56805f6b656d69698f49fe
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2023-12-27 19:19:13 +0100

    Working formik

--- Client/ProfesiNetClient/src/feauture/posts/form/PostForm.tsx
+++ Client/ProfesiNetClient/src/feauture/posts/form/PostForm.tsx
@@ -1,40 +1,44 @@
 import React, { FC, useState } from 'react';
-import { Button, Modal,TextArea, Icon, Grid, Segment, Image } from 'semantic-ui-react';
+import { Button, Modal, TextArea, Icon, Grid, Segment, Image } from 'semantic-ui-react';
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../app/stores/Store";
-import { Formik, Form as FormikForm, Field } from "formik";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from 'yup';
 
 interface FormValues {
     description: string;
     file: File | null;
 }
+
 const PostForm: FC = () => {
     const [open, setOpen] = useState(false);
-    const [file, setFile] = useState<File | null>(null);
     const [previewUrl, setPreviewUrl] = useState<string | null>(null);
     const { postStore } = useStore();
     const { createPost, loading } = postStore;
-    
-    const handleCancelImage = () => {
-        setFile(null);
-        setPreviewUrl(null);
-        const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-        fileInput.value = '';
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>, setFieldValue: (field: string, value: any) => void) => {
+        const file = event.target.files ? event.target.files[0] : null;
+        setFieldValue('file', file);
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreviewUrl(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setPreviewUrl(null);
+        }
     };
 
-    const handleSubmit = async (
-        values: FormValues,
-        { resetForm }: { resetForm: () => void }
-    ): Promise<void> => {
-        const postData = {
-            description: values.description,
-            file: file,
-        };
+    const handleCancelImage = (setFieldValue: (field: string, value: any) => void) => {
+        setPreviewUrl(null);
+        setFieldValue('file', null);
+    };
 
+    const handleSubmit = async (values: FormValues, { resetForm }: { resetForm: () => void }): Promise<void> => {
         try {
-            await createPost(postData);
+            await createPost(values);
             resetForm();
-            setFile(null);
             setPreviewUrl(null);
             setOpen(false);
         } catch (error) {
@@ -43,6 +47,12 @@ const PostForm: FC = () => {
         }
     };
 
+    const validationSchema = Yup.object({
+        description: Yup.string().nullable(),
+        file: Yup.mixed().nullable(),
+    }).test('fileOrDescription', 'Either a description or file is required', value => {
+        return !!(value.description || (value.file instanceof File && value.file.size > 0));
+    });
     return (
         <Grid centered={true}>
             <Button size={"large"} style={{width: "80%"}} onClick={() => setOpen(true)} icon labelPosition='left' primary>
@@ -66,32 +76,22 @@ const PostForm: FC = () => {
                             file: null,
                         }}
                         onSubmit={handleSubmit}
+                        validationSchema={validationSchema}
                     >
-                        {({ setFieldValue, isSubmitting, handleSubmit }) => (
-                            <FormikForm onSubmit={handleSubmit} className='ui form'>
+                        {({ setFieldValue, isSubmitting }) => (
+                            <Form className='ui form'>
                                 <Field name="description" as={TextArea} rows={3} placeholder="What's on your mind?" style={{ minHeight: 200 }} />
                                 <input
                                     id="fileInput"
                                     name="file"
                                     type="file"
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                        const file = event.target.files ? event.target.files[0] : null;
-                                        setFieldValue('file', file).then(r => console.log(r));
-                                        if (file) {
-                                            const reader = new FileReader();
-                                            reader.onloadend = () => {
-                                                setPreviewUrl(reader.result as string);
-                                            };
-                                            reader.readAsDataURL(file);
-                                        }
-                                    }}
+                                    onChange={(event) => handleFileChange(event, setFieldValue)}
                                     hidden
                                 />
-                           
                                 {previewUrl && (
                                     <Segment>
                                         <Image src={previewUrl} size='big' centered />
-                                        <Button icon onClick={handleCancelImage}>
+                                        <Button icon onClick={() => handleCancelImage(setFieldValue)}>
                                             <Icon name='cancel' />
                                         </Button>
                                     </Segment>
@@ -103,7 +103,9 @@ const PostForm: FC = () => {
                                 <Button type='submit' color='green' loading={isSubmitting || loading}>
                                     Publish
                                 </Button>
-                            </FormikForm>
+                                <ErrorMessage name={'description'} render={(error) => <Segment inverted color='red'>{error}</Segment>} />
+                                <ErrorMessage name={'file'} render={(error) => <Segment inverted color='red'>{error}</Segment>} />
+                            </Form>
                         )}
                     </Formik>
                 </Modal.Content>
