c55f38378553a4372992a7094f486fc15b9b4225
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2024-01-05 22:33:37 +0100

    filter the dates of experience and education

--- Api/modules/Users/ProfesiNet.Users.Application/Educations/Queries/GetAll/GetAllUserEducationsQueryHandler.cs
+++ Api/modules/Users/ProfesiNet.Users.Application/Educations/Queries/GetAll/GetAllUserEducationsQueryHandler.cs
@@ -1,7 +1,9 @@
-﻿using MediatR;
+﻿using Confab.Shared.Abstractions.Interfaces;
+using MediatR;
 using ProfesiNet.Shared.Exceptions;
 using ProfesiNet.Users.Application.Educations.Dtos;
 using ProfesiNet.Users.Application.Educations.Mappings;
+using ProfesiNet.Users.Domain.Entities;
 using ProfesiNet.Users.Domain.Exceptions;
 using ProfesiNet.Users.Domain.Interfaces;
 
@@ -11,12 +13,14 @@ internal class GetAllUserEducationsQueryHandler : IRequestHandler<GetAllUserEduc
 {
     private readonly IEducationRepository _educationRepository;
     private readonly IUserRepository _userRepository;
+    private readonly IClock _clock;
     private static readonly EducationMapper Mapper = new();
 
-    public GetAllUserEducationsQueryHandler(IEducationRepository educationRepository, IUserRepository userRepository)
+    public GetAllUserEducationsQueryHandler(IEducationRepository educationRepository, IUserRepository userRepository, IClock clock)
     {
         _educationRepository = educationRepository;
         _userRepository = userRepository;
+        _clock = clock;
     }
     public async Task<IReadOnlyCollection<EducationDto>> Handle(GetAllUserEducationsQuery request, CancellationToken cancellationToken)
     {
@@ -26,6 +30,8 @@ internal class GetAllUserEducationsQueryHandler : IRequestHandler<GetAllUserEduc
             throw new UserNotFoundException(request.Id);
         }
         var educations = await _educationRepository.GetAllForConditionAsync(e => e.UserId == request.Id, cancellationToken);
-        return Mapper.MapEducationDtosToEducations(educations);
+        var enumerable = educations as Education?[] ?? educations.ToArray();
+        var filteredEducations = enumerable.AsQueryable().OrderByDescending(e => e.StartDate).ToList();
+        return Mapper.MapEducationDtosToEducations(filteredEducations);
     }
 }
