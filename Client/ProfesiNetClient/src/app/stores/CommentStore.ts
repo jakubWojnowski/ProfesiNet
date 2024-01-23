873d69370a15813e315cecd2c89ef0bca7655c78
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2024-01-23 23:04:59 +0100

    .

--- Client/ProfesiNetClient/src/app/stores/CommentStore.ts
+++ Client/ProfesiNetClient/src/app/stores/CommentStore.ts
@@ -29,7 +29,7 @@ export default class CommentStore {
                 runInAction(() => this.comments = comments);
 
             })
-            this.hubConnection.on('ReceiveComment', (comment: ChatComment) => {
+            this.hubConnection.on('n', (comment: ChatComment) => {
                 runInAction(() =>   this.comments.unshift(comment)
                 );
             })
